f17ec7bfdb60664c6de023f521a4bd701915dcd1
VWesp <valentin.wesp@uni-jena.de>
2019-06-26 18:02:44 +0200

    updated loading screen

--- PisA.py
+++ PisA.py
@@ -6,24 +6,41 @@ if __name__ == "__main__":
     mp.freeze_support()
 
     import appJar
+    import time
     app = appJar.gui("PisA", "380x400")
     app.winIcon = None
     with app.subWindow("Loading PisA..."):
-        app.addImage("LoadingSun", "icon/loading_sun.gif")
+        with app.labelFrame("Loading image"):
+            app.addImage("LoadingSun", "icon/loading_sun.gif")
+
+        with app.labelFrame("Loading"):
+            app.addEmptyLabel("Loading")
 
     app.showSubWindow("Loading PisA...")
 
+    app.setLabel("Loading", "Loading phototaxisPlotter module...")
     import phototaxisPlotter
+    app.setLabel("Loading", "Loading os module...")
     import os
+    app.setLabel("Loading", "Loading shutil module...")
     import shutil
+    app.setLabel("Loading", "Loading subprocess module...")
     import subprocess
+    app.setLabel("Loading", "Loading pandas module...")
     import pandas as pd
+    app.setLabel("Loading", "numpy phototaxisPlotter module...")
     import numpy as np
+    app.setLabel("Loading", "Loading traceback module...")
     import traceback
+    app.setLabel("Loading", "Loading itertools module...")
     import itertools
+    app.setLabel("Loading", "Loading functools module...")
     from functools import partial
+    app.setLabel("Loading", "Loading PyPDF2 module...")
     from PyPDF2 import PdfFileMerger, PdfFileReader
+    app.setLabel("Loading", "Loading tkinter module...")
     from tkinter import *
+    app.setLabel("Loading", "Building GUI...")
 
     data = None
     header = 1
@@ -1024,7 +1041,7 @@ if __name__ == "__main__":
         progress = manager.Value("i", 0)
         lock = manager.Lock()
         buildAppJarGUI()
-        app.destroySubWindow("Loading PisA...")
+        app.hideSubWindow("Loading PisA...")
         app.go()
         while(not exitApp):
             mainloop() #tkinter
